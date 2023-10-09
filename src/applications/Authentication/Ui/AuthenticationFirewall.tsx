ffc8a19f7b5908633be4e2b1561828e4d22de99d
Clement-Muth <clementmuth@gmail.com>
2023-10-09 13:48:41 -0700

    fix errors

--- src/applications/Authentication/Ui/AuthenticationFirewall.tsx
+++ src/applications/Authentication/Ui/AuthenticationFirewall.tsx
@@ -1,20 +1,6 @@
-import { getSession } from "next-auth/react";
-
 export interface AuthenticationFirewallProps {
   children: React.ReactNode;
 }
-
-const AccessDenied = ({ children }) => {
-  return (
-    <>
-      <h1>Access Denied</h1>
-      <p>
-        <a href="/api/auth/signin">You must be signed in to view this page</a>
-      </p>
-      {children}
-    </>
-  );
-};
 
 const AuthenticationFirewall = ({ children }: AuthenticationFirewallProps) => {
   return children;
