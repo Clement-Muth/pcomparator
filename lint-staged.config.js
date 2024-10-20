85af611131c71d01a897d2b0696ab596ccf5c31c
Clement-Muth <clementmuth@gmail.com>
2024-10-20 17:21:09 +0200

    chore(lint-staged): add translation:extract lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-  "*.{ts,tsx,js,jsx}": ["yarn format:check .", "yarn lint:check", () => "yarn typescript:check --skipLibCheck -p tsconfig.json", ]
+  "*.{ts,tsx,js,jsx}": ["yarn format:check .", "yarn lint:check", () => "yarn typescript:check --skipLibCheck -p tsconfig.json"],
+  "*": "yarn translation:extract"
 }
