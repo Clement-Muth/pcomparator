64780751924669570adee35488f25d6c2581b011
Clement-Muth <clementmuth@gmail.com>
2024-11-01 19:49:13 +0100

    add husky in pcomparator

--- pcomparator/lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-  "*.{ts,tsx,js,jsx}": ["yarn format:check .", "yarn lint:check", () => "yarn typescript:check --skipLibCheck -p tsconfig.json"],
-  "*": "yarn translation:extract"
-}
