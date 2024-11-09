0c3dbaa4961f60ce778ee478e6d3012d35a19c64
Clément Muth <clementmuth@gmail.com>
2024-11-09 20:22:51 +0100

    fix(release): add repositoryUrl for new domaine (#72)

--- release.config.js
+++ release.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  branches: ["dev"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "./CHANGELOG.md",
-      },
-    ],
-    "@semantic-release/git",
-    "@semantic-release/github",
-  ],
-  dryRun: false,
+	branches: ["dev"],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/changelog",
+			{
+				changelogFile: "./CHANGELOG.md",
+			},
+		],
+		"@semantic-release/git",
+		"@semantic-release/github",
+	],
+	dryRun: false,
+	repositoryUrl: "https://github.com/Clement-Muth/deazl.git",
 };
