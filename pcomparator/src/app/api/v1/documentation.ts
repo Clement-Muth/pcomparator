8b0b00c3ac21cae131cceac6fd41023ed3149e63
Clement-Muth <clementmuth@gmail.com>
2024-11-10 21:52:59 +0100

    feat(pcomparator): implement searchbar for prices

--- pcomparator/src/app/api/v1/documentation.ts
+++ pcomparator/src/app/api/v1/documentation.ts
@@ -1,4 +1,5 @@
 import { paths as pathPrices } from "~/app/api/v1/prices/documentation";
+import { paths as pathPriceSearch } from "~/app/api/v1/prices/search/documentation";
 import { paths as pathAccount } from "~/app/api/v1/user/[id]/account/documentation";
 import { paths as pathDeleteUserPrice } from "~/app/api/v1/user/[id]/prices/[priceId]/documentation";
 import { paths as pathUserPrices } from "~/app/api/v1/user/[id]/prices/documentation";
@@ -11,7 +12,8 @@ const routePaths = {
   ...pathProfile,
   ...pathProfileAvatar,
   ...pathUserPrices,
-  ...pathDeleteUserPrice
+  ...pathDeleteUserPrice,
+  ...pathPriceSearch
 };
 
 export { routePaths as paths };
---
+++ pcomparator/src/app/api/v1/prices/search/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { PrismaSearchRepository } from "~/applications/Searchbar/Infrastructure/Repositories/PrismaSearchRepository";
+import { errorHandler } from "~/core/errorHandler";
+import { withAuthentication } from "~/libraries/nextauth/authConfig";
+import { HttpStatus } from "~/types/httpError";
+
+const SearchRepository = new PrismaSearchRepository();
+
+export const GET = withAuthentication(
+  errorHandler(async (request, ctx): Promise<NextResponse> => {
+    const { searchParams } = new URL(request.url);
+    const search = searchParams.get("q") as string;
+
+    const prices = await SearchRepository.search(search);
+
+    return NextResponse.json(prices, { status: HttpStatus.OK });
+  })
+);
